37b5d153ce61ff3f67153257cebfe78d19245702
Furkan Toprak <furkancemaltoprak@gmail.com>
2020-10-17 00:03:50 -0500

    Basic layout.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
   return (
     <div>
-        Home Page
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        home.
+      </h1>
+      <Container>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i: number) => {
+          return (
+            <Row>
+              <Col
+                style={{
+                  backgroundColor: i % 2 === 0 ? "white" : "black",
+                }}>1</Col>
+                <Col
+                style={{
+                  backgroundColor: i % 2 === 0 ? "red" : "blue",
+                }}>2</Col>
+              <Col
+                style={{
+                  backgroundColor: i % 2 === 0 ? "purple" : "yellow",
+                }}>3</Col>
+            </Row>
+          );
+        })}
+      </Container>
     </div>
   );
 }
